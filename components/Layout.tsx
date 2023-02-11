637370d5e69cb793de7c2601c0480fc7f58c2343
happyuniv <scv0930@daum.net>
2023-02-12 07:45:11 +0900

    Create layout component and update _app & _document

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from 'react'
+import Header from './Header'
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+
+      <style jsx global>
+        {`
+          body {
+          }
+        `}
+      </style>
+    </>
+  )
+}
