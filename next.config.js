7522af66c04368f839f5cd33612ecc37679f9661
happyuniv <scv0930@daum.net>
2023-03-16 21:34:16 +0900

    Update. next config to get clodinary image and rewrite upload api

--- next.config.js
+++ next.config.js
@@ -1,18 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/upload',
+        destination: `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "replicate.com",
+        protocol: 'https',
+        hostname: 'replicate.com',
       },
       {
-        protocol: "https",
-        hostname: "replicate.delivery",
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
       },
     ],
-  }
+  },
 }
 
 module.exports = nextConfig
